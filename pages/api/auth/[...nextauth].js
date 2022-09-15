0d733f6fe150ec407d672e915f3dc35f5be3f76f
brentbagapuro <brentbagapuro@gmail.com>
2022-09-15 19:10:47 +0800

    setup google oauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,22 +11,7 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-        params: {
-          prompt: 'consent',
-          access_type: 'offline',
-          response_type: 'code',
-        },
-      },
     }),
   ],
   database: process.env.DATABASE_URL,
-  callbacks: {
-    async signIn({ account, profile }) {
-      if (account.provider === 'google') {
-        return profile.email_verified && profile.email.endsWith('@example.com')
-      }
-      return true // Do different verification for other providers that don't have `email_verified`
-    },
-  },
 })
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import { useSession, signIn, signOut } from 'next-auth/react'
+
 const Home = () => {
+  const { data } = useSession()
+  console.log(data)
   return (
     <div>
-      Hello
+      <button onClick={() => signIn()}>Sign In</button>
     </div>
   )
 }
